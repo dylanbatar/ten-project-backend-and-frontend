f7ddc29bb0d0cad9d38293ead5a7112e6e456fd3
jmestrelozano <jmestrelozano@gmail.com>
2020-04-30 18:53:29 -0500

    Cambios finalizados

--- frontend/blog/js/admin.js
+++ frontend/blog/js/admin.js
@@ -36,3 +36,21 @@ function eliminarDatos() {
 
 
 }
+
+tinymce.init({
+    selector: '#mytextarea',
+    toolbar: "image",
+    plugins: "image imagetools",
+    height: 300,
+    menubar: true,
+    plugins: [
+        'advlist autolink lists link image charmap print preview anchor',
+        'searchreplace visualblocks code fullscreen',
+        'insertdatetime media table paste code help wordcount'
+    ],
+    toolbar: 'undo redo | formatselect | ' +
+        'bold italic backcolor | alignleft aligncenter ' +
+        'alignright alignjustify | bullist numlist outdent indent | ' +
+        'removeformat | help',
+    content_css: '//www.tiny.cloud/css/codepen.min.css'
+});
